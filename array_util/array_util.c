23d9692ead303523028adec6c0f25388c40a7487
spreeti <spreeti@INspreeti.local>
2016-01-05 17:12:59 +0530

    added findIndex function in array_util.c
    What changes have you made?
    
    * create insertElement and findIndex function
    
    Why do we need this change?
    
    * to insert element and find index of a particular element
    
    What can this change impact?
    
    * easily to find or access particular element

--- array_util/array_util.c
+++ array_util/array_util.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "array_util.h"
+#include <string.h>
 
 
 ArrayUtil create(int typeSize, int length){
-		ArrayUtil *util_arr = (ArrayUtil *)calloc(length,length*typeSize);
-		util_arr->base = (void *)util_arr;
+		ArrayUtil *util_arr = (ArrayUtil *)malloc(length*typeSize);
+		util_arr->base = (void *)calloc(length,typeSize);
 		util_arr->typeSize = typeSize;
 		util_arr->length = length;	
+		util_arr->used_length = 0;
 		return *util_arr;
 };
 
@@ -26,9 +28,18 @@ ArrayUtil resize(ArrayUtil *util, int len){
 
 void dispose(ArrayUtil *util){
 	free(util->base);
-	util->base = NULL;
 };
 
+void insert_element(ArrayUtil * util, void * element,int index){
+	void *base = ((util->base)+((util->typeSize)*index));
+	memcpy(base, element, util->typeSize);
+};
 
-
+int find_index(ArrayUtil util,void *element){
+	for (int i = 0; i < util.length; ++i){
+		if(memcmp(util.base+(i*util.typeSize), element, util.typeSize)==0)
+			return i;
+	}
+	return -1;
+}
 
--- array_util/array_util.h
+++ array_util/array_util.h
@@ -2,9 +2,13 @@ typedef struct{
 	void *base;
 	int typeSize;
 	int length;
+	int used_length;
 }ArrayUtil;
+
 
 int areEqual(ArrayUtil, ArrayUtil);
 ArrayUtil create(int, int);
 ArrayUtil resize(ArrayUtil *, int);
 void dispose(ArrayUtil *);
+void insert_element(ArrayUtil *, void *, int);
+int find_index(ArrayUtil , void *);
