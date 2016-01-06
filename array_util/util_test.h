3ec98876a7830bf7e785f142e22c622685588361
spreeti <spreeti@INspreeti.local>
2016-01-06 11:40:03 +0530

    removed unneccessary file

--- array_util/util_test.h
+++
@@ -1,9 +0,0 @@
-void test_array();
-void test_resize_array();
-void test_equal_array_function();
-void test_not_equal_array();
-void test_dispose();
-void find_index_test();
-void test_find_first();
-void test_find_last();
-void count_element_test();
